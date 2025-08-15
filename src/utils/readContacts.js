fb9319b2d7da9407d76b7fd4eee5cedd71b1245a
kamila <sugakillersxs@gmail.com>
2025-08-15 22:32:56 +0200

    add all

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,9 @@
+import { readFile } from 'node:fs/promises';
+
 import { PATH_DB } from '../constants/contacts.js';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  const productsJSON = await readFile(PATH_DB, 'utf-8');
+
+  return productsJSON ? JSON.parse(productsJSON) : [];
+};
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,6 @@
+import { writeFile } from 'node:fs/promises';
+
 import { PATH_DB } from '../constants/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = (updatedContacts) =>
+  writeFile(PATH_DB, JSON.stringify(updatedContacts, null, 2));
